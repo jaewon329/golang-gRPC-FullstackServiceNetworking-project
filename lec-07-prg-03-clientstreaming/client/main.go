2d257f79661dba7079e1e8baa3ddb9f37671e188
jaewon329 <jwon0057@naver.com>
2025-12-04 22:15:28 +0900

    client에서 server로 보낼 메시지를 만드는 generate_messages함수 생성, 서버에 보내고 응답을 받아올 send_message함수 생성

--- lec-07-prg-03-clientstreaming/client/main.go
+++ lec-07-prg-03-clientstreaming/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	pb "lec-07-prg-03-clientstreaming/proto"
 	"log"
 
@@ -8,11 +9,52 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-//보낼 메시지를 만드는 generate_messages함수
+// 보낼 메시지를 만드는 generate_messages함수
+func generate_messages() []string {
+	return []string{
+		"message #1",
+		"message #2",
+		"message #3",
+		"message #4",
+		"message #5",
+	}
+}
 
 // 서버에 보내고 응답을 받아올 send_message함수
 func send_message(stub pb.ClientStreamingClient) {
+	//stub을 이용해서 스트림을 보내기 위한 연결을 생성
+	responses, err := stub.GetServerResponse(context.Background())
+	if err != nil {
+		//에러시 err내용을 출력하고 프로그램 즉시 종료
+		log.Fatalf("fail: %v", err)
+	}
 
+	// 스트림으로 보낼 문자열을 generate_messages함수로 받아온다.
+	msg := generate_messages()
+
+	// 받아온 것 하나씩 꺼내서 보낸다.
+	// 스트림으로 보낼 것이 문자열. 인덱스는 무시하고 문자열 값만으로 반복문 실행
+	for _, messages := range msg {
+		// 서버로 보낼 것 먼저 출력
+		log.Printf("[Client to server] %s\n", messages)
+
+		//messages(보낼 문자열)을 proto 파일에서 정의한 객체 형태로
+		request := &pb.Message{Message: messages}
+		//서버로 Send를 이용해서 보낸다.
+		if err := responses.Send(request); err != nil {
+			//보내는 과정에서 에러시 err내용을 출력하고 프로그램 즉시 종료
+			log.Fatalf("send fail: %v", err)
+		}
+	}
+	// 다 보내면 다 보냈다고 서버에게 알리고(Close), 서버로 부터 응답을 받아온다(Recv)
+	response, err := responses.CloseAndRecv()
+	if err != nil {
+		// 에러시 err내용을 출력하고 프로그램 즉시 종료
+		log.Fatalf("receive fail: %v", err)
+	}
+
+	// 받아온 서버로부터의 결과를 출력
+	log.Printf("[Server to client] %d\n", response.Value)
 }
 
 // 클라이언트를 구동하는 main함수 생성
